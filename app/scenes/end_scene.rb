e0284153963f14b6ce4cf2d55376009d8ff07663
Giovanni Albuquerque <giovannialbuquerquegato@hotmail.com>
2024-02-25 20:41:49 -0300

    Modified end scene to only show death count and added new sprite

--- app/scenes/end_scene.rb
+++ app/scenes/end_scene.rb
@@ -1,11 +1,9 @@
 class EndScene < Scene
 
-  def initialize(deaths,time)
+  def initialize(deaths)
     @deaths = deaths
-    @time = time
     @borders = {x: 2, y: 2, w: 80, h: 44}
     @tick_count = 0
-    @phrases = $gtk.deserialize_state("levels/end_quotes.txt")
   end
 
   def next_scene
@@ -24,12 +22,9 @@ class EndScene < Scene
 
   def input
     @over = true if inputs.keyboard.key_down.enter
-
-    # @deaths += 1 if inputs.keyboard.key_down.right
-    # @deaths -= 1 if inputs.keyboard.key_down.left
-    # @time += 600 if inputs.keyboard.key_down.up
-    # @time -= 600 if inputs.keyboard.key_down.down
-
+    #@deaths += 1 if inputs.keyboard.key_down.right
+    #@deaths -= 1 if inputs.keyboard.key_down.left
+    @deaths = 0 if @deaths < 0
   end
 
   def render
@@ -38,58 +33,22 @@ class EndScene < Scene
     right = @borders.x + @borders.w
     args.nokia.borders <<  @borders
 
-    args.nokia.labels << args.nokia
-                              .default_label
-                              .merge(x: right - 2,
-                                     y: top - 4, text: "FINISHED!",
-                                     alignment_enum: 2, size_enum: NOKIA_FONT_SM)
+    args.nokia.sprites << {
+        x: 18, y: 28, w: 50, h: 15, path: "sprites/you_are_on_fire.png"
+      }
 
     args.nokia.labels << args.nokia
-                              .default_label
-                              .merge(x: @borders.x + 2,
-                                     y: top - 12, text: "Deaths: #{@deaths}",
-                                     alignment_enum: 0, size_enum: NOKIA_FONT_SM)
-
-    args.nokia.labels << args.nokia
-                             .default_label
-                             .merge(x: @borders.x + 5,
-                                    y: top - 18, text: get_phrase(@phrases.deaths, @deaths),
-                                    alignment_enum: 0, size_enum: NOKIA_FONT_SM)
-    minutes = (@time/(3600)).floor
-    seconds = (@time/(60)).floor % 60
-    minutes = 99 if minutes > 99
-    seconds = 99 if minutes > 99
-    time_str = "Time: %02d:%02d"%[minutes,seconds]
-    args.nokia.labels << args.nokia
-                             .default_label
-                             .merge(x: @borders.x + 2,
-                                    y: top - 24, text: time_str,
-                                    alignment_enum: 0, size_enum: NOKIA_FONT_SM)
-
-    args.nokia.labels << args.nokia
-                             .default_label
-                             .merge(x: @borders.x + 5,
-                                    y: top - 30, text: get_phrase(@phrases.time, @time),
-                                    alignment_enum: 0, size_enum: NOKIA_FONT_SM)
-
+                              .default_label  
+                              .merge(x: middle_x,
+                                      y: top - 24, text: "DEATHS: #{@deaths}",
+                                      alignment_enum: 1, size_enum: NOKIA_FONT_SM)
 
     if (args.state.tick_count/30)%3 > 1
-      args.nokia.labels << args.nokia
-                               .default_label
-                               .merge(x: middle_x,
-                                      y: @borders.y+6, text: "Press Enter",
-                                      alignment_enum: 1, size_enum: NOKIA_FONT_SM)
+      args.nokia.sprites << {
+        x: 18, y: 4, w: 48, h: 8, path: "sprites/press_enter.png"
+      }
     end
 
-  end
-
-  def get_phrase phrases, var
-    key = 50
-    for k in phrases.keys
-      if k <= var
-        key = k
-      end
-    end
-    return phrases[key]
+    
   end
 end
