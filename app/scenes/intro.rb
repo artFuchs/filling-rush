1ec9869fcd783f47bcd6e2e8f30e193bfecae8f5
Arthur L. Fuchs <alfuchs17@gmail.com>
2024-02-26 01:22:13 -0300

    added credists in intro. fixed position of tutorial on level1

--- app/scenes/intro.rb
+++ app/scenes/intro.rb
@@ -13,15 +13,30 @@ class Intro < Scene
         @over = true  if keyboard.key_down.enter
         @time += 1
 
+        time_to_start = 180
+
         key_frames = [0,5,11,17,22,26,30,34,36,39,42,47]
 
-        fps = 8
-        c_frame = @time.idiv(fps)
+        c_frame = 0
+
+        if @time > time_to_start
+            c_frame = (@time - time_to_start).idiv(8)
+        end
+
         c_keyframe = key_frames.find_all{|k| k < c_frame}.last
         
         frame_str = c_keyframe.to_s.rjust(4,'0');
         path = "sprites/intro/frame#{frame_str}.png"
         args.nokia.sprites << {x: 0, y: 0, w: 84, h: 48, path: path}
+
+
+        if @time < 60
+            args.nokia.sprites << {x: 0, y: 0, w: 84, h: 48, path: "sprites/intro/credits_dr.png"} 
+        elsif @time < time_to_start && @time > 70
+            args.nokia.sprites << {x: 0, y: 0, w: 84, h: 48, path: "sprites/intro/credits_creators.png"} 
+        end
+            
+
 
         if c_frame == 5
             play_sound args, :intro0005
