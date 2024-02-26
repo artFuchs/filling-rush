95993178ad24db639070a3b5fac4b066715dcd54
Arthur L. Fuchs <alfuchs17@gmail.com>
2024-02-26 01:06:07 -0300

    added ending and credits images to the intro

--- app/scenes/end_scene.rb
+++ app/scenes/end_scene.rb
@@ -3,7 +3,7 @@ class EndScene < Scene
   def initialize(deaths)
     @deaths = deaths
     @borders = {x: 2, y: 2, w: 80, h: 44}
-    @tick_count = 0
+    @time = 0
   end
 
   def next_scene
@@ -11,44 +11,40 @@ class EndScene < Scene
   end
 
   def tick
-    if @tick_count == 0
+    if @time == 0
       play_sound args, :start
     end
-
-    @tick_count += 1
     render
     input
+    @time += 1
   end
 
   def input
     @over = true if inputs.keyboard.key_down.enter
-    #@deaths += 1 if inputs.keyboard.key_down.right
-    #@deaths -= 1 if inputs.keyboard.key_down.left
     @deaths = 0 if @deaths < 0
   end
 
   def render
-    top = @borders.y + @borders.h
-    middle_x = @borders.x + @borders.w/2
-    right = @borders.x + @borders.w
-    args.nokia.borders <<  @borders
 
-    args.nokia.sprites << {
-        x: 18, y: 28, w: 50, h: 15, path: "sprites/you_are_on_fire.png"
-      }
+    @thanks_x ||= 84
 
-    args.nokia.labels << args.nokia
-                              .default_label  
-                              .merge(x: middle_x,
-                                      y: top - 24, text: "DEATHS: #{@deaths}",
-                                      alignment_enum: 1, size_enum: NOKIA_FONT_SM)
+    key_frames = [0,4,7,8,9,10,11,14,18,20,23,25,29]
 
-    if (args.state.tick_count/30)%3 > 1
-      args.nokia.sprites << {
-        x: 18, y: 4, w: 48, h: 8, path: "sprites/press_enter.png"
-      }
+    c_frame = @time.idiv(15)
+    c_keyframe = key_frames.find_all{|k| k < c_frame}.last
+    
+    frame_str = c_keyframe.to_s.rjust(4,'0');
+    path = "sprites/ending/frame#{frame_str}.png"
+
+    args.nokia.sprites << {x: @thanks_x - 84, y: 0, w: 84, h: 48, path: path}
+
+    if c_keyframe == 0
+      goal_frame = args.state.tick_count.idiv(30)%2
+      args.nokia.sprites << {x: 53, y: 33, w: 8, h: 8, path: "sprites/exit#{goal_frame}.png"}
     end
 
-    
+    return if c_frame < 35
+    @thanks_x -= 1 if @thanks_x > 0
+    args.nokia.sprites << {x: @thanks_x, y: 0, w: 84, h: 48, path: "sprites/ending/thanks.png"}
   end
 end
