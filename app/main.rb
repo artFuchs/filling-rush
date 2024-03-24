82cd42cde89935171f099a2f1dd659b721f66c78
Giovanni Albuquerque <giovannialbuquerquegato@hotmail.com>
2024-03-24 15:57:30 -0300

    Refactored main.rb

--- app/main.rb
+++ app/main.rb
@@ -8,30 +8,28 @@ require 'app/scenes/level_editor.rb'
 
 $level_box = {x: -1, y: 0, w: 86, h: 49, r: 64, g: 82, b: 61}
 $scene = nil
-def tick args
 
+def tick args
   if $scene
     $scene.args = args
     $scene.tick
     if $scene.over
-      next_scene = $scene.next_scene
-      $scene = next_scene
+      $scene = $scene.next_scene
     end
   else
-    set_scene
+    $scene = Intro.new()
   end
 
-
-  if args.inputs.keyboard.key_held.minus # if minus key is held
-    args.gtk.slowmo!(3) # Slow motion
+  # Debugging: Slow motion (Hold minus key)
+  if args.inputs.keyboard.key_held.minus
+    args.gtk.slowmo!(3)
   end
+
+  # Debugging: Switch debug details (Press equal key)
   if args.inputs.keyboard.key_down.equal
     args.state.render_debug_details ||= false
     args.state.render_debug_details = ! args.state.render_debug_details 
   end
-  #args.outputs.debug << args.gtk.framerate_diagnostics_primitives
-end
 
-def set_scene
-  $scene = Intro.new()
+  #args.outputs.debug << args.gtk.framerate_diagnostics_primitives
 end
